a0392f16d8046b3d25a6b07a2f9f21e8fe04ccfe
P4L4K <121123428+P4L4K@users.noreply.github.com>
2025-08-01 00:20:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import tensorflow
+from PIL import Image
+import numpy as np
+
+st.write('Classifier')
+classes = ['Cat','Dog']
+
+@st.cache_resource
+
+def load_model():
+    model = tensorflow.keras.models.load_model(r"cat_dog_model_u.h5")
+    return model
+
+model = load_model()
+
+def preprocess(image:Image.Image):
+    img = image.resize((128,128))
+    img = np.array(img)
+    img = img.reshape(1,128,128,3)
+    return img
+
+upload_f = st.file_uploader('Choose an image', type = ['jpg', 'jpeg','png'])
+if upload_f:
+    st.write('File Upload')
+    image = Image.open(upload_f)
+    st.image(image, caption = 'Uploaded Images')
+    preprocessed= preprocess(image)
+    pred=model.predict(preprocessed)[0]
+    class_index = np.argmax(pred)
+    pred_class = classes[class_index]
+    st.success(f'Predicted Class: {pred_class} ')
